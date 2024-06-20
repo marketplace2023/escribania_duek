a527b95432ace2f5a24e0b40a318273a3f9cbb47
Alexis Navarro <alexisnavarro1994@gmail.com>
2024-06-20 13:45:15 -0400

    Extract components common to auth module

--- src/modules/auth/pages/register.tsx
+++ src/modules/auth/pages/register.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/modules/shared/components/ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import loginImage from "@/assets/login_shirt.png";
 import {
   Tabs,
   TabsContent,
@@ -8,73 +7,41 @@ import {
   TabsTrigger,
 } from "@/modules/shared/components/ui/tabs";
 import RegisterForm from "../components/register/register-form";
+import { Link } from "react-router-dom";
+import AuthTitleSection from "../components/ui/auth-title-section";
 
 const Register = () => {
   return (
-    <div className="container">
-      <div className="grid grid-cols-2">
-        <div className="w-full max-w-[450px]">
-          <div className="mb-6">
-            <h2 className="text-3xl text-primary mb-3">Regístrate</h2>
-            <p className="text-gray-500">
-              Acceso gratuito a todas las herramientas de tu estampa
-            </p>
-          </div>
+    <>
+      <AuthTitleSection title="Regístrate" />
 
-          <Tabs defaultValue="customer">
-            <div className="flex justify-between mb-3">
-              <Button variant="outline" size="icon">
-                <GitHubLogoIcon className="h-4 w-4" />
-              </Button>
+      <Tabs defaultValue="customer">
+        <div className="flex justify-between mb-3">
+          <Button variant="outline" size="icon">
+            <GitHubLogoIcon className="h-4 w-4" />
+          </Button>
 
-              <TabsList>
-                <TabsTrigger value="customer">Comprador</TabsTrigger>
-                <TabsTrigger value="store">Vendedor</TabsTrigger>
-              </TabsList>
-            </div>
-            <TabsContent value="customer">
-              <RegisterForm />
-            </TabsContent>
-
-            <TabsContent value="store">
-              <h2>The stores form</h2>
-            </TabsContent>
-          </Tabs>
-
-          <p className="text-xs">
-            ¿Ya tienes una cuenta?{" "}
-            <a href="#" className="text-primary font-semibold">
-              Iniciar sesión
-            </a>
-          </p>
+          <TabsList>
+            <TabsTrigger value="customer">Comprador</TabsTrigger>
+            <TabsTrigger value="store">Vendedor</TabsTrigger>
+          </TabsList>
         </div>
+        <TabsContent value="customer">
+          <RegisterForm />
+        </TabsContent>
 
-        <div className="flex flex-col items-end">
-          <div
-            className="w-full max-w-[400px] p-3 aspect-[3/4] flex flex-col justify-between border-2 border-primary rounded-md bg-contain bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${loginImage})` }}
-          >
-            <p className="p-2 bg-primary/80 text-3xl text-primary-foreground text-center rounded-md">
-              10% Menos despues de registrarte
-            </p>
-            <p className="p-2 bg-primary/80 text-3xl text-primary-foreground text-center rounded-md">
-              Tienda DeTodito
-            </p>
-          </div>
+        <TabsContent value="store">
+          <h2>The stores form</h2>
+        </TabsContent>
+      </Tabs>
 
-          <div className="w-full max-w-[400px] mt-3 space-y-2">
-            <p className="text-xl font-semibold">
-              ¡Precios hasta 30% más bajos con Tu Estampa!
-            </p>
-            <p>
-              Accede a beneficios exclusivos con nuesto plan mensual. Cientos de
-              clientes ya han probado y han aumentado sus ventas más de un 25%.
-            </p>
-            <p>¡Empieza tu prueba ahora!</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      <p className="text-xs">
+        ¿Ya tienes una cuenta?{" "}
+        <Link to="/login" className="text-primary font-semibold">
+          Iniciar sesión
+        </Link>
+      </p>
+    </>
   );
 };
 
